35a47e42496028a06d087ec7fe6a123f36bb3d15
Abhishek A B <bsheks@gmail.com>
2026-02-09 01:47:08 +0530

    Created the welcome page.

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { StyledEngineProvider } from '@mui/material/styles';
+import GlobalStyles from '@mui/material/GlobalStyles';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <StyledEngineProvider enableCssLayer>
+  <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
     <App />
+  </StyledEngineProvider>
   </StrictMode>,
 )
--- Frontend/src/pages/Welcome.jsx
+++ Frontend/src/pages/Welcome.jsx
@@ -1,24 +1,51 @@
 import { Link } from "react-router-dom";
+import ListIcon from '@mui/icons-material/List';
+import TextField from "@mui/material/TextField";
 
 export default function Welcome() {
     return (
         <div className="bg-yellow-400 
         min-h-screen 
-        flex 
-        flex-col
-        items-center 
-        justify-center">
-            <h1 className="text-3xl font-bold text-white">Welcome to AB Task Manager</h1>
-            <button className="mt-4 
-            px-4
-            py-2 
-            bg-indigo-400 
-            text-white
-            ease-in-out duration-200
-            rounded hover:bg-blue-600
-            hover:-translate-y-1 hover:scale-105 hover:bg-indigo-500">
-                <Link to="/login">Login</Link>
-            </button>
+        p-2">
+            <div className="flex justify-between">
+                <ListIcon className="text-6xl text-white" />
+                <h1 className="text-4xl font-bold text-white">Welcome to your personal Task Manager</h1>
+                <button className="mt-2 
+                px-2
+                py-4 
+                max-w-min
+                max-h-min
+                bg-indigo-400 
+                text-white
+                ease-in-out duration-200
+                rounded hover:bg-blue-600
+                hover:-translate-y-1 hover:scale-105 hover:bg-indigo-500">
+                    <Link to="/login">Login</Link>
+                </button>
+            </div>
+
+            <div className="flex flex-col justify-center items-center m-10
+            outline-1 outline-gray-500 outline-dashed rounded-lg p-10
+            bg-amber-400">
+                <h2 className="text-2xl font-bold text-gray-700">Organize your tasks, boost your productivity, and achieve your goals with ease.</h2>
+                <form className="flex flex-col justify-center items-center">
+                <TextField label="First Name" variant="outlined" required className="min-w-8 mt-4" />
+                <TextField label="Last Name" variant="outlined" className="mt-4" />
+                <TextField label="Email" variant="outlined" required className="mt-4" />
+                <TextField label="Create Password" variant="outlined" required className="mt-4" />
+                <button className="mt-4 
+                px-4
+                py-2 
+                max-w-min
+                bg-indigo-400 
+                text-white
+                ease-in-out duration-200
+                rounded hover:bg-blue-600
+                hover:-translate-y-1 hover:scale-105 hover:bg-indigo-500">
+                    <Link to="/signup">Signup</Link>
+                </button>
+                </form>
+            </div>
         </div>
     )
 }
